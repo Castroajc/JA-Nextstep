adb13b40ac8575e52f08b2796c87b242957ed952
acastro <datmexicanbuy@gmail.com>
2017-05-11 08:59:09 -0400

    v.0.1.3 Got rid of Constraint Layout, which removed a lot of bugs with that trying to put in the blue heading bar at top

--- app/src/main/java/org/pltw/examples/ja_nextstep/MainActivity.java
+++ app/src/main/java/org/pltw/examples/ja_nextstep/MainActivity.java
@@ -1,9 +1,10 @@
 package org.pltw.examples.ja_nextstep;
 
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -15,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
 
         //this is the start of the code that makes the Webview work
         mWebView = (WebView)findViewById(R.id.webView);
@@ -40,4 +44,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
